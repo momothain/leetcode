67067e8a77c1801363e445e9ce34c6d25fce5533
Morgann Thain <mthain@cs.brown.edu>
2025-10-15 16:22:01 -0400

    d

---
+++ easy/232-queue-via-stacks.py
@@ -0,0 +1,61 @@
+class MyQueue:
+    # Class variables are shared btw all instances and don't reset on creation and such
+    # s1 = []
+    # s2 = []
+    # s1_has_elts = False
+    # pop_is_fifo = False
+
+    def __init__(self):
+        # print("INIT")
+        # print(f"s1,s2: {self.s1, self.s2}")
+        self.s1 = []
+        self.s2 = []
+        self.s1_has_elts = False
+        self.pop_is_fifo = False
+        return
+        
+    def get_elts(self) -> tuple[list,list]:
+        return (self.s1, self.s2) if self.s1_has_elts else (self.s2, self.s1)
+    
+    def flip_elts(self) -> None:
+        print("FLIP START")
+        elts, empty_list = self.get_elts()
+        print(f"elts, empty_list: {elts, empty_list}")
+        # if not pop_is_fifo:
+        while elts:
+            empty_list.append(elts.pop())
+
+
+        self.s1_has_elts = not self.s1_has_elts
+        self.pop_is_fifo = not self.pop_is_fifo
+        print(f"elts, empty_list: {elts, empty_list}")
+
+
+    def push(self, x: int) -> None:
+        print(f"self.pop_is_fifo: {self.pop_is_fifo}")
+        if self.pop_is_fifo:
+            self.flip_elts()
+        elts, empty_list = self.get_elts()
+        elts.append(x)
+        print(f"elts: {elts}")
+
+    def pop(self) -> int:
+        print("POP")
+        print(f"self.pop_is_fifo: {self.pop_is_fifo}")
+        if not self.pop_is_fifo:
+            self.flip_elts()
+        
+        elts, empty_list = self.get_elts()
+        return elts.pop()
+
+    def peek(self) -> int:
+        if not self.pop_is_fifo:
+            self.flip_elts()
+        elts, empty_list = self.get_elts()
+        return elts[-1]
+        
+
+    def empty(self) -> bool:
+        elts, empty_list = self.get_elts()
+        print(elts, empty_list)
+        return not bool(elts)
